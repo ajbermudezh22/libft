9065acac8a8cc2e24e775ccda3336821bea487cb
Alberto Bermudez <101905065+ajbermudezh22@users.noreply.github.com>
2023-06-20 23:01:01 +0200

    Add files via upload

---
+++ ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albermud <albermud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 21:25:09 by albermud          #+#    #+#             */
+/*   Updated: 2023/05/10 19:20:22 by albermud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <limits.h>
+#include "libft.h"
+
+int	ft_isspace(int c)
+{
+	return ((c >= 9 && c <= 13) || c == 32);
+}
+
+int	ft_atoi(const char *str)
+{
+	int		sign;
+	long	ret;
+
+	ret = 0;
+	sign = 1;
+	while (ft_isspace(*str))
+		++str;
+	if (*str == '+' || *str == '-')
+		if (*(str++) == '-')
+			sign *= -1;
+	while (ft_isdigit(*str))
+	{
+		ret = ret * 10 + sign * (*str++ - '0');
+		if (ret > INT_MAX)
+			return (-1);
+		else if (ret < INT_MIN)
+			return (0);
+	}
+	return ((int)ret);
+}
